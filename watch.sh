60ffb166d31d7d6780ee28cff374ea7fa9a2eca0
Vykintas Narmontas (William) <me@vynar.com>
2017-02-03 08:22:27 +0800

    Update watch.sh

--- watch.sh
+++ watch.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-inotifywait -m -e close_write,moved_to,create  . |
-while read -r directory events filename; do
-  if [ "$events" = "CLOSE_WRITE,CLOSE" ]; then
-  	if [[ "$filename" == *.pub ]; then
+dom() {
       python ~/Incubator/cumb.py
       scp users.db assaultcube@woop.ac:/home/assaultcube/latest/ag/config/users
       scp groups.db assaultcube@woop.ac:/home/assaultcube/latest/ag/config/groups
+
+}
+dom
+inotifywait -m -e close_write,moved_to,create  . |
+while read -r directory events filename; do
+  if [ "$events" = "CLOSE_WRITE,CLOSE" ]; then
+  	if [[ "$filename" == *.pub ]]; then
+dom
     fi
 fi
 done
